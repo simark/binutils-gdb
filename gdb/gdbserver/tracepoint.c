8fd4375ffbefe1206b0577f1fc0af4557ccf3786
Par Olsson <par.olsson@windriver.com>
2016-01-28 13:03:11 -0500

    Fix endian problem for tracepoint enabled flag
    When running big endian machines there is a problem with
    the enabled flag for tracepoints as it is defined as a
    int8_t but written from gdbserver as an integer and then
    read in the agent as 8-bit value.
    This caused problem when tracepoint was disabled and
    re-enabled.
    
    gdb/gdbserver/ChangeLog:
    
    	* tracepoint.c (struct tracepoint): Make sure to write
    	enable flag as 8-bit value.
    
    Signed-off-by: Par Olsson <par.olsson@windriver.com>
    Signed-off-by: Henrik Wallin <henrik.wallin@windriver.com>

--- gdb/gdbserver/tracepoint.c
+++ gdb/gdbserver/tracepoint.c
@@ -460,6 +460,12 @@ write_inferior_uinteger (CORE_ADDR symaddr, unsigned int val)
   return write_inferior_memory (symaddr, (unsigned char *) &val, sizeof (val));
 }
 
+static int
+write_inferior_char (CORE_ADDR symaddr, char val)
+{
+  return write_inferior_memory (symaddr, (unsigned char *) &val, sizeof (val));
+}
+
 static CORE_ADDR target_malloc (ULONGEST size);
 
 #define COPY_FIELD_TO_BUF(BUF, OBJ, FIELD)	\
@@ -2651,7 +2657,7 @@ cmd_qtdv (char *own_buf)
 }
 
 static void
-cmd_qtenable_disable (char *own_buf, int enable)
+cmd_qtenable_disable (char *own_buf, int8_t enable)
 {
   char *packet = own_buf;
   ULONGEST num, addr;
@@ -2695,7 +2701,7 @@ cmd_qtenable_disable (char *own_buf, int enable)
 	      return;
 	    }
 	  
-	  ret = write_inferior_integer (obj_addr, enable);
+	  ret = write_inferior_char (obj_addr, enable);
 	  done_accessing_memory ();
 	  
 	  if (ret)
