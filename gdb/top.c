1a7084fc56e875fbc287255b475d44b7ab09dd7f
Pedro Alves <palves@redhat.com>
2016-04-18 19:27:12 +0100

    Add new command to create extra console/mi UI channels
    With all the previous plumbing in place, it's now easy to add a
    command that actually creates a new console/mi UI.
    
    The intended use case is to make it possible and easy for MI frontends
    to provide a fully featured GDB console to users, with readline
    support, command line editing, history, etc., just like if gdb was
    started on the command line.  Currently MI frontends have to try to
    implement all of that theirselves and make use of "-interpreter-exec
    console ...", which is far from perfect.  If you ever tried Eclipse's
    gdb console window, you'll know what I mean...
    
    Instead of trying to multiplex console through MI, this command let's
    just leverage all the built in readline/editing support already inside
    gdb.
    
    The plan is for the MI frontend to start GDB in regular console mode,
    running inside a terminal emulator widget embedded in Eclipse (which
    already exists, for supporting the shell widget; other frontends have
    similar widgets), and then tell GDB to run a full MI interpreter on an
    extra / separate side channel, independent of the console.
    
    My original prototype planned to do things the other way around --
    start GDB in MI mode, and then start an extra CLI console on separate
    tty.  I handed over that prototype to Marc Khouzam @ Eclipse CDT, and
    after experimentation and discussion, we ended up concluding that
    starting GDB in CLI mode instead was both easier and actually also
    supported an interesting use case -- connect an Eclipse frontend to a
    GDB that is already running outside Eclipse.
    
    The current usage is "new-ui <interpreter> <tty>".
    
    E.g., on a terminal run this scriplet:
    
     $ cat gdb-client
     #!/bin/bash
    
     reset
     tty
     tail -f /dev/null
    
     $ gdb-client
     /dev/pts/15
    
    Now run gdb on another terminal, and tell it to start a MI interpreter
    on the tty of the other terminal:
    
     ...
     (gdb) new-ui mi /dev/pts/15
     New UI allocated
    
    Now back to the the gdb-client terminal, we'll get an MI prompt, ready
    for MI input:
    
     /dev/pts/15
     =thread-group-added,id="i1"
     (gdb)
    
    You can also start a new UI running a CLI, with:
    
     (gdb) new-ui console /dev/pts/15
    
    Though note that this console won't support readline command editing
    (yet).  It works as if "set editing off" was entered.
    
    Starting more than one MI UI is probably very broken, because there
    are still globals in the MI code that should be made per UI.  Should
    be trivial to fix, though much lower priority than getting one MI
    working.

--- gdb/top.c
+++ gdb/top.c
@@ -74,6 +74,10 @@
 # include "tui/tui.h"
 #endif
 
+#ifndef O_NOCTTY
+# define O_NOCTTY 0
+#endif
+
 extern void initialize_all_files (void);
 
 #define PROMPT(X) the_prompts.prompt_stack[the_prompts.top + X].prompt
@@ -312,6 +316,73 @@ delete_ui (struct ui *todel)
     ui_list = ui->next;
 
   free_ui (ui);
+}
+
+static FILE *
+open_stream (const char *name)
+{
+  int fd;
+
+  fd = open (name, O_RDWR | O_NOCTTY);
+  if (fd < 0)
+    perror_with_name  (_("opening terminal failed"));
+
+  return fdopen (fd, "w+");
+}
+
+static void
+new_ui_command (char *args, int from_tty)
+{
+  struct ui *ui;
+  struct interp *interp;
+  FILE *stream[3] = { NULL, NULL, NULL };
+  int i;
+  int res;
+  int argc;
+  char **argv;
+  const char *interpreter_name;
+  const char *tty_name;
+  struct cleanup *back_to;
+  struct cleanup *streams_chain;
+
+  argv = gdb_buildargv (args);
+  back_to = make_cleanup_freeargv (argv);
+  argc = countargv (argv);
+
+  if (argc < 2)
+    error (_("usage: new-ui <interpreter> <tty>"));
+
+  interpreter_name = argv[0];
+  tty_name = argv[1];
+
+  streams_chain = make_cleanup (null_cleanup, NULL);
+
+  /* Open specified terminal, once for each of
+     stdin/stdout/stderr.  */
+  for (i = 0; i < 3; i++)
+    {
+      stream[i] = open_stream (tty_name);
+      make_cleanup_fclose (stream[i]);
+    }
+
+  ui = new_ui (stream[0], stream[1], stream[2]);
+
+  discard_cleanups (streams_chain);
+
+  initialize_stdin_serial (ui);
+  ui->async = 1;
+
+  make_cleanup (restore_ui_cleanup, current_ui);
+  current_ui = ui;
+
+  set_top_level_interpreter (interpreter_name);
+
+  interp_pre_command_loop (top_level_interpreter ());
+
+  /* This restores the previous UI.  */
+  do_cleanups (back_to);
+
+  printf_unfiltered ("New UI allocated\n");
 }
 
 /* Handler for SIGHUP.  */
@@ -1887,6 +1958,8 @@ set_history_filename (char *args, int from_tty, struct cmd_list_element *c)
 static void
 init_main (void)
 {
+  struct cmd_list_element *c;
+
   /* Initialize the prompt to a simple "(gdb) " prompt or to whatever
      the DEFAULT_PROMPT is.  */
   set_prompt (DEFAULT_PROMPT);
@@ -2010,6 +2083,12 @@ When set, GDB uses the specified path to search for data files."),
                            set_gdb_datadir, show_gdb_datadir,
                            &setlist,
                            &showlist);
+
+  c = add_cmd ("new-ui", class_support, new_ui_command, _("\
+Create a new UI.  It takes two arguments:\n\
+The first argument is the name of the interpreter to run.\n\
+The second argument is the terminal the UI runs on.\n"), &cmdlist);
+  set_cmd_completer (c, interpreter_completer);
 }
 
 void
