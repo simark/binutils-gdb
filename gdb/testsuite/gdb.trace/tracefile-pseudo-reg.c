ef7f06a407291b44c1b5a6cdfb016bf459406a79
Antoine Tremblay <antoine.tremblay@ericsson.com>
2016-08-29 12:43:53 -0400

    Enable tracing of pseudo-registers on ARM
    This patch implements the ax_pseudo_register_push_stack and
    ax_pseudo_register_collect gdbarch functions so that a pseudo-register can
    be traced.
    
    No regressions, tested on ubuntu 14.04 ARMv7 and x86.
    With gdbserver-{native,extended} / { -marm -mthumb }
    
    gdb/ChangeLog:
    
    	* arm-tdep.c (arm_pseudo_register_to_register): New function.
    	(arm_ax_pseudo_register_collect): New function.
    	(arm_ax_pseudo_register_push_stack): New function.
    	(arm_gdbarch_init): Set
    	gdbarch_ax_pseudo_register_{collect,push_stack} functions.
    
    gdb/testsuite/ChangeLog:
    
    	* gdb.trace/tracefile-pseudo-reg.c: Include arm_neon.h if on arm.
    	(main): Add a register variable and a tracepoint label.
    	* gdb.trace/tracefile-pseudo-reg.exp: Add arm pseudo register
    	tracing test with s5 pseudo register.

--- gdb/testsuite/gdb.trace/tracefile-pseudo-reg.c
+++ gdb/testsuite/gdb.trace/tracefile-pseudo-reg.c
@@ -15,12 +15,14 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
-/*
- * Test program for reading target description from tfile: collects AVX
- * registers on x86_64.
- */
+/* Test program for reading target description from tfile: collects pseudo
+   registers on the target.  */
 
+#if (defined __x86_64__)
 #include <immintrin.h>
+#elif (defined __arm__)
+#include <stdint.h>
+#endif
 
 void
 dummy (void)
@@ -35,6 +37,7 @@ end (void)
 int
 main (void)
 {
+#if (defined __x86_64__)
   /* Strictly speaking, it should be ymm15 (xmm15 is 128-bit), but gcc older
      than 4.9 doesn't recognize "ymm15" as a valid register name.  */
   register __v8si a asm("xmm15") = {
@@ -48,6 +51,11 @@ main (void)
     0x12340008,
   };
   asm volatile ("traceme: call dummy" : : "x" (a));
+#elif (defined __arm__)
+  register uint32_t a asm("s5") = 0x3f800000; /* 1. */
+  asm volatile ("traceme: bl dummy" : : "x" (a));
+#endif
+
   end ();
   return 0;
 }
