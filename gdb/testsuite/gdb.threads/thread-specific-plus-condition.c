826c37a144f9a0887ef24a92f69da056b007dce1
Simon Marchi <simon.marchi@ericsson.com>
2017-03-30 11:42:18 -0400

    Add new test for thread-specific breakpoints with conditions
    While developing the gdbserver-based thread-specific breakpoints, I had
    a few bugs in my implementation, which lead me to write these tests.  I
    think it would be useful to have them in the testsuite, especially since
    they can also help people who write stubs verify that they handle that
    case correctly.
    
    gdb/testsuite/ChangeLog:
    
    	* gdb.threads/thread-specific-plus-condition.exp: New file.
    	* gdb.threads/thread-specific-plus-condition.c: New file.

---
+++ gdb/testsuite/gdb.threads/thread-specific-plus-condition.c
@@ -0,0 +1,78 @@
+/* This testcase is part of GDB, the GNU debugger.
+
+   Copyright 2017 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#include <pthread.h>
+#include <assert.h>
+
+static pthread_barrier_t barrier;
+static int x = 0;
+
+static void *
+thread_2_func (void *varg)
+{
+  int i;
+
+  /* Tell the main thread the thread has started.  */
+  pthread_barrier_wait (&barrier);
+
+  /* Wait until main tells us we can start looping.  */
+  pthread_barrier_wait (&barrier);
+
+  for (i = 0; i < 100; i++)
+    x++; /* thread_2_func loop tag */
+
+  return NULL;
+}
+
+static void
+start (pthread_t *thread, void *(*func)(void *))
+{
+  int res = pthread_create (thread, NULL, func, NULL);
+  assert (res == 0);
+}
+
+static void
+join (pthread_t thread)
+{
+  int res = pthread_join (thread, NULL);
+  assert (res == 0);
+}
+
+int
+main (void)
+{
+  pthread_t thread2;
+  int res;
+
+  pthread_barrier_init (&barrier, NULL, 2);
+
+  start (&thread2, thread_2_func);
+
+  /* Wait until the thread has started.  */
+  pthread_barrier_wait (&barrier);
+
+  /* thread started tag */
+
+  /* Tell the thread it can start looping.  */
+  pthread_barrier_wait (&barrier);
+  
+  join (thread2);
+  
+  /* main done tag */
+
+  return 0;
+}
