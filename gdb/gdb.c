db2028e778d1e330d51820c8b6efa5e0d8599ee3
Simon Marchi <simon.marchi@ericsson.com>
2017-05-29 14:30:47 +0200

    Breakpad prototype

--- gdb/gdb.c
+++ gdb/gdb.c
@@ -20,14 +20,39 @@
 #include "main.h"
 #include "interps.h"
 
+#include <client/linux/handler/exception_handler.h>
+
+static bool
+cb (const  google_breakpad::MinidumpDescriptor& descriptor, void* context, bool succeeded)
+{
+  int pid = fork ();
+
+  if (pid == 0) {
+      const char *exec_path = "/bin/echo";
+      const char *args[] = {
+	  exec_path,
+	  "Dump written to: ",
+	  descriptor.path(),
+	  NULL,
+      };
+
+      execv (exec_path, (char **) args);
+  }
+
+  return succeeded;
+}
+
 int
 main (int argc, char **argv)
 {
   struct captured_main_args args;
 
+  google_breakpad::MinidumpDescriptor descriptor ("/tmp");
+  google_breakpad::ExceptionHandler eh(descriptor, NULL, cb, NULL, true, -1);
+
   memset (&args, 0, sizeof args);
   args.argc = argc;
-  args.argv = argv;
+  args.argv = NULL;
   args.interpreter_p = INTERP_CONSOLE;
   return gdb_main (&args);
 }
