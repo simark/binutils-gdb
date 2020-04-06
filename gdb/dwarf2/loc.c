fa10558ac5e7e576cca28a99ff38f03a6d24448c
Simon Marchi <simon.marchi@polymtl.ca>
2020-04-06 13:01:05 -0400

    Add dwarf2_per_objfile parameter to get_die_type_at_offset
    This allows removing some dwarf2_per_cu_data::dwarf2_per_objfile
    references.
    
    gdb/ChangeLog:
    
    	* dwarf2/read.h (dwarf2_get_die_type): Add dwarf2_per_objfile
    	parameter.
    	* dwarf2/read.c (get_die_type_at_offset): Likewise.
    	(read_namespace_alias): Update.
    	(lookup_die_type): Update.
    	(dwarf2_get_die_type): Add dwarf2_per_objfile parameter.
    	* dwarf2/loc.c (class dwarf_evaluate_loc_desc) <get_base_type>:
    	Update.
    	(disassemble_dwarf_expression): Update.

--- gdb/dwarf2/loc.c
+++ gdb/dwarf2/loc.c
@@ -673,7 +673,7 @@ public:
 
   struct type *get_base_type (cu_offset die_offset, int size) override
   {
-    struct type *result = dwarf2_get_die_type (die_offset, per_cu);
+    struct type *result = dwarf2_get_die_type (die_offset, per_cu, per_objfile);
     if (result == NULL)
       error (_("Could not find type for DW_OP_const_type"));
     if (size != 0 && TYPE_LENGTH (result) != size)
@@ -4110,7 +4110,7 @@ disassemble_dwarf_expression (struct ui_file *stream,
 
 	    data = safe_read_uleb128 (data, end, &ul);
 	    cu_offset offset = (cu_offset) ul;
-	    type = dwarf2_get_die_type (offset, per_cu);
+	    type = dwarf2_get_die_type (offset, per_cu, per_objfile);
 	    fprintf_filtered (stream, "<");
 	    type_print (type, "", stream, -1);
 	    fprintf_filtered (stream, " [0x%s]> %d",
@@ -4126,7 +4126,7 @@ disassemble_dwarf_expression (struct ui_file *stream,
 
 	    data = safe_read_uleb128 (data, end, &ul);
 	    cu_offset type_die = (cu_offset) ul;
-	    type = dwarf2_get_die_type (type_die, per_cu);
+	    type = dwarf2_get_die_type (type_die, per_cu, per_objfile);
 	    fprintf_filtered (stream, "<");
 	    type_print (type, "", stream, -1);
 	    fprintf_filtered (stream, " [0x%s]>",
@@ -4150,7 +4150,7 @@ disassemble_dwarf_expression (struct ui_file *stream,
 	    data = safe_read_uleb128 (data, end, &ul);
 	    cu_offset type_die = (cu_offset) ul;
 
-	    type = dwarf2_get_die_type (type_die, per_cu);
+	    type = dwarf2_get_die_type (type_die, per_cu, per_objfile);
 	    fprintf_filtered (stream, "<");
 	    type_print (type, "", stream, -1);
 	    fprintf_filtered (stream, " [0x%s]> [$%s]",
@@ -4173,7 +4173,7 @@ disassemble_dwarf_expression (struct ui_file *stream,
 	      {
 		struct type *type;
 
-		type = dwarf2_get_die_type (type_die, per_cu);
+		type = dwarf2_get_die_type (type_die, per_cu, per_objfile);
 		fprintf_filtered (stream, "<");
 		type_print (type, "", stream, -1);
 		fprintf_filtered (stream, " [0x%s]>",
